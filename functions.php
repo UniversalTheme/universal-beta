881cfa678f159939949a3d210e3db446686f73c3
Ulrich Pogson <grapplerulrich@gmail.com>
2013-06-02 23:20:30 +0200

    FIx
    Warning:
    require(/home/grapplerulrich/universaltheme.tk/wp-content/themes/universal-beta/inc/jetpack.php)
    [function.require]: failed to open stream: No such file or directory in
    /home/grapplerulrich/universaltheme.tk/wp-content/themes/universal-beta/functions.php
    on line 241
    
    Fatal error: require() [function.require]: Failed opening required
    '/home/grapplerulrich/universaltheme.tk/wp-content/themes/universal-beta/inc/jetpack.php'
    (include_path='.:/usr/local/lib/php:/usr/local/php5/lib/pear') in
    /home/grapplerulrich/universaltheme.tk/wp-content/themes/universal-beta/functions.php
    on line 241

--- functions.php
+++ functions.php
@@ -22,26 +22,6 @@ if ( ! function_exists( 'universal_setup' ) ) :
 function universal_setup() {
 
 	/**
-	 * Custom template tags for this theme.
-	 */
-	require( get_template_directory() . '/inc/template-tags.php' );
-
-	/**
-	 * Custom functions that act independently of the theme templates
-	 */
-	require( get_template_directory() . '/inc/extras.php' );
-
-	/**
-	 * Custom Theme Options
-	 */
-	require( get_template_directory() . '/inc/theme-options/theme-options.php' );
-
-	/**
-	 * Customizer additions
-	 */
-	require( get_template_directory() . '/inc/customizer.php' );
-
-	/**
 	 * Make theme available for translation
 	 * Translations can be filed in the /languages/ directory
 	 * If you're building a theme based on Universal, use a find and replace
